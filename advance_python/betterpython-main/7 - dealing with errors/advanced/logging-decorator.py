31ebc34d3d415ee085abb54d1934ebaaf4deda5f
Sahil Singh <sahilsingh@Sahils-MacBook-Pro.local>
2025-01-08 08:22:15 +0530

    feat: advance logging decorator

--- advance_python/betterpython-main/7 - dealing with errors/advanced/logging-decorator.py
+++ advance_python/betterpython-main/7 - dealing with errors/advanced/logging-decorator.py
@@ -1,31 +1,33 @@
-import logging 
+import logging
 from functools import wraps
+
 
 # Example from: https://www.geeksforgeeks.org/create-an-exception-logging-decorator-in-python/
 
-def create_logger(): 
-	
-	# create a logger object 
-	logger = logging.getLogger('exc_logger') 
-	logger.setLevel(logging.INFO) 
-	
-	#c reate a file to store all the 
-	# logged exceptions 
-	logfile = logging.FileHandler('exc_logger.log') 
-	
-	fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-	formatter = logging.Formatter(fmt) 
-	
-	logfile.setFormatter(formatter) 
-	logger.addHandler(logfile) 
-	
-	return logger 
+def create_logger():
+    # create a logger object
+    logger = logging.getLogger('exc_logger')
+    logger.setLevel(logging.INFO)
 
-logger = create_logger() 
+    # c reate a file to store all the
+    # logged exceptions
+    logfile = logging.FileHandler('exc_logger.log')
+
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(fmt)
+
+    logfile.setFormatter(formatter)
+    logger.addHandler(logfile)
+
+    return logger
+
+
+logger = create_logger()
 
 # you will find a log file 
 # created in a given path 
-print(logger) 
+print(logger)
+
 
 def exception(logger):
     def decorator(func):
@@ -34,19 +36,21 @@ def exception(logger):
             try:
                 return func(*args, **kwargs)
             except:
-                issue = "exception in "+func.__name__+"\n"
-                issue = issue+"=============\n"
+                issue = "exception in " + func.__name__ + "\n"
+                issue = issue + "=============\n"
                 logger.exception(issue)
                 raise
+
         return wrapper
-    return decorator 
+
+    return decorator
 
 
-@exception(logger) 
-def divideByZero(): 
-	return 12/0
+@exception(logger)
+def divideByZero():
+    return 12 / 0
 
-# Driver Code 
-if __name__ == '__main__': 
-	divideByZero() 
 
+# Driver Code
+if __name__ == '__main__':
+    divideByZero()
